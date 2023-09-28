c570b867f389e63715ca4e84a06d91868121e83a
David Lukens <54252372+Lukens4242@users.noreply.github.com>
2023-09-28 16:35:19 -0400

    Add files via upload
    Adding in parsing for autodetecting insecure flag.

--- crdb.py
+++ crdb.py
@@ -1,6 +1,7 @@
-from sos.report.plugins import Plugin, IndependentPlugin
-import os
+from sos.report.plugins import Plugin, IndependentPlugin, PluginOpt
+import os, sys
 from datetime import datetime
+import subprocess
 
 class crdb(Plugin, IndependentPlugin):
     """
@@ -14,17 +15,33 @@ class crdb(Plugin, IndependentPlugin):
     short_desc = 'CockroachDB debug lite'
 
     plugin_name = "crdb"
+    profiles = ('services',)
+
+    #nuill things out to start with
+    pid = ''
+    cmdline = ''
+    optinsec = False 
+
+    ps_output = subprocess.check_output(['ps', 'aux']).decode('utf-8').split('\n')
+    for line in ps_output:
+        if 'cockroach start' in line.lower():
+            fields = line.split()
+            pid = fields[1]
+            cmdline = ' '.join(fields[10:])
+
+    if "--insecure" in cmdline:
+        optinsec=True
 
     option_list = [
-            ("crdbexec", "full path to the cockroach executable if it is not in your path", '', "cockroach"),
-            ("url", "pgurl for the cluster", '', "postgres://localhost:26257"),
-            ("certsdir", "path to the certificate directory containing the CA and client certs and client key", '', ""),
-            ("insecure", "Set to true if using an insecure cluster", '', False),
-            ("redact", "Set to true to redact the debug zip data", '', False)
+        PluginOpt('crdbexec', desc='full path to the cockroach executable if it is not in your path', default='cockroach'),
+        PluginOpt('url', desc='pgurl for the cluster', default='postgres://localhost:26257'),
+        PluginOpt('certsdir', desc='path to the certificate directory containing the CA and client certs and client key', default=''),
+        PluginOpt('insecure', desc='Set to true if using an insecure cluster', default=optinsec),
+        PluginOpt('redact', desc='Set to true to redact the debug zip data', default=False)
             ]
 
     def setup(self):
-#        crdbout = self.collect_cmd_output("/home/ubuntu/cockroach debug zip --url 'postgres://localhost:26257' --insecure /tmp/debug-crdb.zip", sizelimit=None, suggest_filename="cockroach")
+#       crdbout = self.collect_cmd_output("/home/ubuntu/cockroach debug zip --url 'postgres://localhost:26257' --insecure /tmp/debug-crdb.zip", sizelimit=None, suggest_filename="cockroach")
         url = self.get_option('url')
         certsdir = self.get_option('certsdir')
         crdbexec = self.get_option('crdbexec')
@@ -41,7 +58,7 @@ class crdb(Plugin, IndependentPlugin):
         if self.get_option("redact"):
             cmd = cmd + " --redact"
 
-#        print(cmd)
+#       print(cmd)
         crdbout = self.collect_cmd_output(cmd, sizelimit=None, suggest_filename="cockroach")
         if crdbout['status'] == 0:
             self.add_copy_spec(debugfile, sizelimit=None)
